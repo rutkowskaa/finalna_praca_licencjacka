b2b751ff6d8fcbfbca238a1cda991f2c9c097114
Fatafim <57731567+Fatafim@users.noreply.github.com>
2022-02-17 14:22:15 +0100

    First structure

---
+++ resources/CART_AR.py
@@ -0,0 +1,32 @@
+import Get_Data
+
+class KNN_AR():
+    def __init__(self, data:Get_Data=None, brute_data=None, params:dict()=None, plot_predicted_insample:bool=False):
+        print("Konstruktor")
+        self.fit()
+        self.cross_validation_rolling_window()
+        self.predict()
+        self.forecast_raw()
+        self.forecast()
+        self.analizuj_reszty()
+
+    def fit(self, params:dict()):
+        print("fit")
+
+    def cross_validation_rolling_window(self, dlugosc_okna:int, lags:int=None):
+        print("cross_validation_rolling_window")
+
+    def cross_validation_pruning(self):
+        print("pruning")
+        
+    def predict(self):
+        print("predict")
+
+    def forecast_raw(self):
+        print("forecast_raw")
+
+    def forecast(self):
+        print("forecast")
+
+    def analizuj_reszty(self):
+        print("analizuj_reszty")
--- resources/Get_Data.py
+++ resources/Get_Data.py
@@ -5,7 +5,7 @@ class Get_Data:
     def __init__(self, nazwa_instrumentu:str, start:str, interval:str, days_train:int=30):
         print("Konstruktor")
         self.make_diff()
-        self.analiza_statystyczna_szeregu()
+        self.analiza_statystyczna_szeregu(pd.Series)
 
     def make_diff(self):
         print("make_diff()")
