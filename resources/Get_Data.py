94401520cbc30f93b1b54e5bdce2ba1eb0493a4b
Fatafim <57731567+Fatafim@users.noreply.github.com>
2022-02-17 14:04:22 +0100

    First
    First

---
+++ resources/Get_Data.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import pandas as pd
+
+class Get_Data:
+    def __init__(self, nazwa_instrumentu:str, start:str, interval:str, days_train:int=30):
+        print("Konstruktor")
+        self.make_diff()
+        self.analiza_statystyczna_szeregu()
+
+    def make_diff(self):
+        print("make_diff()")
+
+    @staticmethod
+    def analiza_statystyczna_szeregu(szereg_pandas: pd.Series, max_lag:int = 30, co_sprawdzamy:str="DANE PODSTAWOWE", wykres:bool=True, crit:str="AIC"):
+        print("analiza_statystyczna_szeregu")
+
+getter = Get_Data("asd", "asd", "asd", "123")
